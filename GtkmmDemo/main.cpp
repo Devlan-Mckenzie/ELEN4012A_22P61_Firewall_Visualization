50a27ba5403d1bed8fdf2baecd549a7fe614bff7
Devlan Mckenzie <1876492@students.wits.ac.za>
2022-10-03 12:00:54 +0200

    GtkmmDemo
    -Created a cpp file to test the functionality of the Gtkmm library
    -Additionally confirmed that the IDE being used is correctly linking the library
    -Serves as Gtkmm "Hello World"

---
+++ GtkmmDemo/main.cpp
@@ -0,0 +1,11 @@
+#include <gtkmm.h>
+
+int main(int argc, char **argv) {
+    auto app = Gtk::Application::create(argc,argv, "com.solarianprogrammer");
+
+    Gtk::Window win;
+    win.set_default_size(800,600);
+    win.set_title("Gtkmm 3 Demo");
+
+    return app->run(win);
+}
