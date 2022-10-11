05516f63db4354a3877d98f12fbe9bb2b1a2ac0e
Muchaveleli Manjate <1653288@students.wits.ac.za>
2022-10-11 20:42:25 +0200

    break down the file content string into individual rules

--- Python/hello.py
+++ Python/hello.py
@@ -11,5 +11,21 @@ else:
 
 with open(sys.argv[1], 'r') as f:
     contents = f.read()
- 
-print(contents)
+
+
+rule_list = []
+r = re.compile('')
+rule_list += r.sub("", contents).split("\n")
+
+rules_columns = []
+
+for rule in rule_list:
+    boken_rule = []
+    boken_rule += r.sub("", rule).split(" ")
+    
+    for field in boken_rule:
+        s = field.find('-')
+        c = field in rules_columns
+        if ((s!=-1) and (c == False)):
+            rules_columns.append(field)
+            continue
