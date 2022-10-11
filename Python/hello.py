e817e61c5b9c10f8f18cadab9acb9c00ec3d2839
Muchaveleli Manjate <1653288@students.wits.ac.za>
2022-10-11 20:39:57 +0200

    read in file from terminal

--- Python/hello.py
+++ Python/hello.py
@@ -1,2 +1,15 @@
-msg = "Hello World"
-print(msg)
+import sys
+import csv
+from turtle import update
+import pandas as pd
+import re
+import numpy as np
+if sys.version_info[0] < 3: 
+    from StringIO import StringIO
+else:
+    from io import StringIO
+
+with open(sys.argv[1], 'r') as f:
+    contents = f.read()
+ 
+print(contents)
