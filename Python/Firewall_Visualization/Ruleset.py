f5a8774a54198ef45c46a1b9a4513d31a472ff02
Devlan Mckenzie <1876492@students.wits.ac.za>
2022-10-11 16:37:58 +0200

    Updated filename functionality
    -Changed folder Firewall Visualization to Firewall_Visualization
    -Created user input functionality for the input file names
    -Tested input and reading functionality of file
    -Added a temp rule set text file for testing purposes

--- Python/Firewall_Visualization/Ruleset.py
+++ Python/Firewall_Visualization/Ruleset.py
@@ -9,7 +9,7 @@ class RuleSet:
     Rules = []
 
     def __init__(self,fileName):
-        self.fileName = fileName
+        self.fileName = "Python\\Firewall_Visualization\\" + fileName
     
     def importRules(self):
         print("The importRules function was called successfully")
@@ -17,3 +17,6 @@ class RuleSet:
         newRule = Rule.Rule("192.168.1.1","Accepted")
         self.Rules.append(newRule)
         print(self.Rules[0])
+        #Attempting to read in the textfile 
+        file = open(self.fileName,'rt')
+        print(file.read())
