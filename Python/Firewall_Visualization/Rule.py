39f0b021bc3529a52c38c29b408b416b8300462f
Devlan Mckenzie <1876492@students.wits.ac.za>
2022-10-12 13:13:12 +0200

    Updated rule class to match data frame fields
    -The rule class has all the fields required to integrate with the data frame now

--- Python/Firewall_Visualization/Rule.py
+++ Python/Firewall_Visualization/Rule.py
@@ -1,9 +1,22 @@
 # This class will contain the information relevant to each Firewall rule 
 
 class Rule:
-    def __init__(self,Ip,Status):
-        self.Ip = Ip
-        self.Status = Status
+    def __init__(self,A_Flag,S_Flag,J_Flag,P_Flag,M_Flag,State_Flag,Dport_Flag):
+        # A_Flag shows direction like input or output
+        # S_Flag shows Ip 
+        # J_Flag shows status like Accept or Drop
+        # P_Flag shows protocol like tcp
+        # M_Flag shows state or multiport 
+        # State_Flag shows if a connection has been established 
+        # Dport_Flag shows which port is the destination port like 25 etc 
+
+        self.A_Flag = A_Flag
+        self.S_Flag = S_Flag
+        self.J_Flag = J_Flag
+        self.P_Flag = P_Flag
+        self.M_Flag = M_Flag
+        self.State_Flag = State_Flag
+        self.Dport_Flag = Dport_Flag 
 
     def __str__(self):
-        return f"{self.Ip}({self.Status})"
+        return f"{self.A_Flag,self.S_Flag,self.J_Flag,self.P_Flag,self.M_Flag,self.State_Flag,self.Dport_Flag}"
