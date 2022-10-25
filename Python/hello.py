57d220c5c431e80d9c33b39acd1a9bea010b8c5e
Muchaveleli Manjate <1653288@students.wits.ac.za>
2022-10-25 13:10:27 +0200

    Bugged expressions

--- Python/hello.py
+++ Python/hello.py
@@ -1,5 +1,4 @@
 import sys
-import csv
 from turtle import update
 import pandas as pd
 import re
@@ -11,57 +10,106 @@ else:
 
 with open(sys.argv[1], 'r') as f:
     contents = f.read()
+contents = contents.replace('-','')
+# contents = contents.replace('.','P')
+# contents = contents.replace('/','R')
 
+#import dd.autoref as _bdd
+from dd import autoref as _bdd
 
 rule_list = []
 r = re.compile('')
 rule_list += r.sub("", contents).split("\n")
 
+play_rules = []
 rules_columns = []
 
 for rule in rule_list:
-    boken_rule = []
-    boken_rule += r.sub("", rule).split(" ")
+    broken_rule = []
+    broken_rule += r.sub("", rule).split(" ")
     
-    for field in boken_rule:
+    for field in broken_rule:
         s = field.find('-')
         c = field in rules_columns
         if ((s!=-1) and (c == False)):
             rules_columns.append(field)
             continue
+    play_rules.append(broken_rule)
+print(play_rules)
 
-for col in rules_columns:
-    p=0
-    for rule_ in rule_list:
-        b = rule_.find(col)
-        if b == -1:
-            rule_list[p] = rule_list[p] + col + " $ "
-        p+=1
-print(len(col))
-updated_content =' '.join(map(str,rule_list))
+bdd = _bdd.BDD()
 
-rules = []
-clean_rules = []
-rr = re.compile('\n')
-clean_rules += rr.sub("", updated_content).split(" ")      
+IP_list =[]
+for RULE in play_rules:
+    IP_addr = []
+    for FIELD in RULE:
+        s = FIELD.find('.')
+        if s!=-1 : 
+            IP_addr +=  r.sub("", FIELD).split(".")
+            IP_list.append(IP_addr)
+            for o in IP_addr:
+                bdd.declare(o)
+                #print(o)
+                continue
+        bdd.declare(FIELD)
+        #print(FIELD)
+        #print(FIELD)
+
+for run in play_rules:
+    i = 0
+    rule_exp =[]
+    
+    while i < len(run):
+        a = run[i]
+        b = run[i+1]
+        s= r'{a}/\{b}'.format(b=b,a=a)
+        print(s)
+       # c = bdd.add_expr(s)
+        # print(c)
+        # print(s)
+        i+=2
+        if a == "j":
+            state_ = c
+            continue
+        rule_exp.append(c)
+
+    # n = rule_exp[0]    
+    # rule_bdd = r'{state_} <=> {n}'.format(state_=state_,n=n)
+    # o=bdd.to_expr(rule_bdd)
+#print(play_rules)
+# for col in rules_columns:
+#     p=0
+#     for rule_ in rule_list:
+#         b = rule_.find(col)
+#         if b == -1:
+#             rule_list[p] = rule_list[p] + col + " $ "
+#         p+=1
+
+# updated_content =' '.join(map(str,rule_list))
+
+# rules = []
+# clean_rules = []
+# rr = re.compile('\n')
+# clean_rules += rr.sub("", updated_content).split(" ")      
 
 
-for q in rules_columns:
-    fields = []
-    i=1
-    add = False
-    for x in clean_rules:
-        if x == q:
-            fields.append(clean_rules[i])
-            add = True
-        i+=1
-    rules.append(fields)
+# for q in rules_columns:
+#     fields = []
+#     i=1
+#     add = False
+#     for x in clean_rules:
+#         if x == q:
+#             fields.append(clean_rules[i])
+#             add = True
+#         i+=1
+#     rules.append(fields)
 
-a=0
-dict = {}
-while a < len(rules_columns):
-    dict[rules_columns[a]]=rules[a]
-    a+=1
-#print(col)
-df = pd.DataFrame(dict)
-print(df)
+
+# a=0
+# dict = {}
+# while a < len(rules_columns):
+#     dict[rules_columns[a]]=rules[a]
+#     a+=1
+# #print(col)
+# df = pd.DataFrame(dict)
+
