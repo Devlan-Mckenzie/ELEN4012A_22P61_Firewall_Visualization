241c58401800baf45b79249c8c808c9c6bc60d7d
Muchaveleli Manjate <1653288@students.wits.ac.za>
2022-11-01 19:06:42 +0200

    rule class refactored, general implementation has bugs

--- Python/Firewall_Visualization/Rule.py
+++ Python/Firewall_Visualization/Rule.py
@@ -1,7 +1,8 @@
 # This class will contain the information relevant to each Firewall rule 
 
 class Rule:
-    def __init__(self,A_Flag,S_Flag,J_Flag,P_Flag,M_Flag,State_Flag,Dport_Flag):
+    # def __init__(self,A_Flag,S_Flag,J_Flag,P_Flag,M_Flag,State_Flag,Dport_Flag):
+    def __init__(self,newRule):
         # A_Flag shows direction like input or output
         # S_Flag shows Ip 
         # J_Flag shows status like Accept or Drop
@@ -10,37 +11,53 @@ class Rule:
         # State_Flag shows if a connection has been established 
         # Dport_Flag shows which port is the destination port like 25 etc 
 
-        self.A_Flag = A_Flag
-        self.S_Flag = S_Flag
-        self.J_Flag = J_Flag
-        self.P_Flag = P_Flag
-        self.M_Flag = M_Flag
-        self.State_Flag = State_Flag
-        self.Dport_Flag = Dport_Flag 
+        # self.A_Flag = A_Flag
+        # self.S_Flag = S_Flag
+        # self.J_Flag = J_Flag
+        # self.P_Flag = P_Flag
+        # self.M_Flag = M_Flag
+        # self.State_Flag = State_Flag
+        # self.Dport_Flag = Dport_Flag 
+        flagPair = {}
+        i=0
+        while i<len(newRule):
+            a = newRule[i]
+            b = newRule[i+1]
+            flagPair[a]=b
+            i+=2
+        self.ruleFlags = flagPair
+        
+            
 
-    def __str__(self):
-        return f"{self.A_Flag,self.S_Flag,self.J_Flag,self.P_Flag,self.M_Flag,self.State_Flag,self.Dport_Flag}"
+
+    # def __str__(self):
+    #     return f"{self.A_Flag,self.S_Flag,self.J_Flag,self.P_Flag,self.M_Flag,self.State_Flag,self.Dport_Flag}"
     
-    def getA_Flag(self):
-        return self.A_Flag
+    def __str__(self):
+        return f"{self.ruleFlags}"
 
-    def getS_Flag(self):
-        return self.S_Flag
+    def getFlag(self,flag):
+        return self.ruleFlags[flag]
+    # def getA_Flag(self):
+    #     return self.A_Flag
 
-    def getJ_Flag(self):
-        return self.J_Flag
+    # def getS_Flag(self):
+    #     return self.S_Flag
 
-    def getP_Flag(self):
-        return self.P_Flag
+    # def getJ_Flag(self):
+    #     return self.J_Flag
 
-    def getM_Flag(self):
-        return self.M_Flag
+    # def getP_Flag(self):
+    #     return self.P_Flag
 
-    def getState_Flag(self):
-        return self.State_Flag
+    # def getM_Flag(self):
+    #     return self.M_Flag
 
-    def getDport_Flag(self):
-        return self.Dport_Flag
+    # def getState_Flag(self):
+    #     return self.State_Flag
+
+    # def getDport_Flag(self):
+    #     return self.Dport_Flag
 
     def getIPBreakDownByPart(self,part):
         # Takes in an interger as variable part and returns the spot as string
@@ -56,7 +73,7 @@ class Rule:
                 return
             
             # Break apart the string using '.' as a delimiter 
-            parts = self.S_Flag.split(".")
+            parts = self.ruleFlags['-s'].split(".")
             if(part == 1):
                 return parts[0]    
             if(part == 2):
